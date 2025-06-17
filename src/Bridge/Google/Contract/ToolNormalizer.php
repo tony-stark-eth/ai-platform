php-llm/llm-chain

feat: add Google Gemini tool support (#331)
Add tool support to Google Gemini.

Extracted from #320 with updates after #326

Co-authored-by: Vin Souza <vin@8sistemas.com>

src/Platform/Bridge/Google/Contract/ToolNormalizer.php
@@ -1,0 +1,54 @@
+<?php
+
+namespace PhpLlm\LlmChain\Platform\Bridge\Google\Contract;
+
+use PhpLlm\LlmChain\Platform\Bridge\Google\Gemini;
+use PhpLlm\LlmChain\Platform\Contract\JsonSchema\Factory;
+use PhpLlm\LlmChain\Platform\Contract\Normalizer\ModelContractNormalizer;
+use PhpLlm\LlmChain\Platform\Model;
+use PhpLlm\LlmChain\Platform\Tool\Tool;
+
+/**
+ * @author Valtteri R <valtzu@gmail.com>
+ *
+ * @phpstan-import-type JsonSchema from Factory
+ */
+final class ToolNormalizer extends ModelContractNormalizer
+{
+    protected function supportedDataClass(): string
+    {
+        return Tool::class;
+    }
+
+    protected function supportsModel(Model $model): bool
+    {
+        return $model instanceof Gemini;
+    }
+
+    /**
+     * @param Tool $data
+     *
+     * @return array{
+     *     functionDeclarations: array{
+     *         name: string,
+     *         description: string,
+     *         parameters: JsonSchema|array{type: 'object'}
+     *     }[]
+     * }
+     */
+    public function normalize(mixed $data, ?string $format = null, array $context = []): array
+    {
+        $parameters = $data->parameters;
+        unset($parameters['additionalProperties']);
+
+        return [
+            'functionDeclarations' => [
+                [
+                    'description' => $data->description,
+                    'name' => $data->name,
+                    'parameters' => $parameters,
+                ],
+            ],
+        ];
+    }
+}

src/Platform/Bridge/Google/ModelHandler.php
@@ -7,9 +7,13 @@ namespace PhpLlm\LlmChain\Platform\Bridge\Google;
 use PhpLlm\LlmChain\Platform\Exception\RuntimeException;
 use PhpLlm\LlmChain\Platform\Model;
 use PhpLlm\LlmChain\Platform\ModelClientInterface;
+use PhpLlm\LlmChain\Platform\Response\Choice;
+use PhpLlm\LlmChain\Platform\Response\ChoiceResponse;
 use PhpLlm\LlmChain\Platform\Response\ResponseInterface as LlmResponse;
 use PhpLlm\LlmChain\Platform\Response\StreamResponse;
 use PhpLlm\LlmChain\Platform\Response\TextResponse;
+use PhpLlm\LlmChain\Platform\Response\ToolCall;
+use PhpLlm\LlmChain\Platform\Response\ToolCallResponse;
 use PhpLlm\LlmChain\Platform\ResponseConverterInterface;
 use Symfony\Component\HttpClient\EventSourceHttpClient;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
@@ -52,6 +56,12 @@ final readonly class ModelHandler implements ModelClientInterface, ResponseConve
 
         $generationConfig = ['generationConfig' => $options];
         unset($generationConfig['generationConfig']['stream']);
+        unset($generationConfig['generationConfig']['tools']);
+
+        if (isset($options['tools'])) {
+            $generationConfig['tools'] = $options['tools'];
+            unset($options['tools']);
+        }
 
         return $this->httpClient->request('POST', $url, [
             'headers' => [
@@ -76,11 +86,22 @@ final readonly class ModelHandler implements ModelClientInterface, ResponseConve
 
         $data = $response->toArray();
 
-        if (!isset($data['candidates'][0]['content']['parts'][0]['text'])) {
+        if (!isset($data['candidates'][0]['content']['parts'][0])) {
             throw new RuntimeException('Response does not contain any content');
         }
 
-        return new TextResponse($data['candidates'][0]['content']['parts'][0]['text']);
+        /** @var Choice[] $choices */
+        $choices = array_map($this->convertChoice(...), $data['candidates']);
+
+        if (1 !== \count($choices)) {
+            return new ChoiceResponse(...$choices);
+        }
+
+        if ($choices[0]->hasToolCall()) {
+            return new ToolCallResponse(...$choices[0]->getToolCalls());
+        }
+
+        return new TextResponse($choices[0]->getContent());
     }
 
     private function convertStream(ResponseInterface $response): \Generator
@@ -114,12 +135,68 @@ final readonly class ModelHandler implements ModelClientInterface, ResponseConve
                     throw new RuntimeException('Failed to decode JSON response', 0, $e);
                 }
 
-                if (!isset($data['candidates'][0]['content']['parts'][0]['text'])) {
+                /** @var Choice[] $choices */
+                $choices = array_map($this->convertChoice(...), $data['candidates'] ?? []);
+
+                if (!$choices) {
                     continue;
                 }
 
-                yield $data['candidates'][0]['content']['parts'][0]['text'];
+                if (1 !== \count($choices)) {
+                    yield new ChoiceResponse(...$choices);
+                    continue;
+                }
+
+                if ($choices[0]->hasToolCall()) {
+                    yield new ToolCallResponse(...$choices[0]->getToolCalls());
+                }
+
+                if ($choices[0]->hasContent()) {
+                    yield $choices[0]->getContent();
+                }
             }
         }
+    }
+
+    /**
+     * @param array{
+     *     finishReason?: string,
+     *     content: array{
+     *         parts: array{
+     *             functionCall?: array{
+     *                 id: string,
+     *                 name: string,
+     *                 args: mixed[]
+     *             },
+     *             text?: string
+     *         }[]
+     *     }
+     * } $choice
+     */
+    private function convertChoice(array $choice): Choice
+    {
+        $contentPart = $choice['content']['parts'][0] ?? [];
+
+        if (isset($contentPart['functionCall'])) {
+            return new Choice(toolCalls: [$this->convertToolCall($contentPart['functionCall'])]);
+        }
+
+        if (isset($contentPart['text'])) {
+            return new Choice($contentPart['text']);
+        }
+
+        throw new RuntimeException(\sprintf('Unsupported finish reason "%s".', $choice['finishReason']));
+    }
+
+    /**
+     * @param array{
+     *     id: string,
+     *     name: string,
+     *     args: mixed[]
+     * } $toolCall
+     */
+    private function convertToolCall(array $toolCall): ToolCall
+    {
+        return new ToolCall($toolCall['id'] ?? '', $toolCall['name'], $toolCall['args']);
     }
 }

tests/Platform/Bridge/Google/Contract/AssistantMessageNormalizerTest.php
@@ -9,7 +9,9 @@ use PhpLlm\LlmChain\Platform\Bridge\Google\Gemini;
 use PhpLlm\LlmChain\Platform\Contract;
 use PhpLlm\LlmChain\Platform\Message\AssistantMessage;
 use PhpLlm\LlmChain\Platform\Model;
+use PhpLlm\LlmChain\Platform\Response\ToolCall;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
@@ -20,6 +22,7 @@ use PHPUnit\Framework\TestCase;
 #[UsesClass(Gemini::class)]
 #[UsesClass(AssistantMessage::class)]
 #[UsesClass(Model::class)]
+#[UsesClass(ToolCall::class)]
 final class AssistantMessageNormalizerTest extends TestCase
 {
     #[Test]
@@ -41,14 +44,33 @@ final class AssistantMessageNormalizerTest extends TestCase
         self::assertSame([AssistantMessage::class => true], $normalizer->getSupportedTypes(null));
     }
 
+    #[DataProvider('normalizeDataProvider')]
     #[Test]
-    public function normalize(): void
+    public function normalize(AssistantMessage $message, array $expectedOutput): void
     {
         $normalizer = new AssistantMessageNormalizer();
-        $message = new AssistantMessage('Great to meet you. What would you like to know?');
 
         $normalized = $normalizer->normalize($message);
 
-        self::assertSame([['text' => 'Great to meet you. What would you like to know?']], $normalized);
+        self::assertSame($expectedOutput, $normalized);
+    }
+
+    /**
+     * @return iterable<string, array{AssistantMessage, array{text?: string, functionCall?: array{id: string, name: string, args?: mixed}}[]}>
+     */
+    public static function normalizeDataProvider(): iterable
+    {
+        yield 'assistant message' => [
+            new AssistantMessage('Great to meet you. What would you like to know?'),
+            [['text' => 'Great to meet you. What would you like to know?']],
+        ];
+        yield 'function call' => [
+            new AssistantMessage(toolCalls: [new ToolCall('id1', 'name1', ['arg1' => '123'])]),
+            [['functionCall' => ['id' => 'id1', 'name' => 'name1', 'args' => ['arg1' => '123']]]],
+        ];
+        yield 'function call without parameters' => [
+            new AssistantMessage(toolCalls: [new ToolCall('id1', 'name1')]),
+            [['functionCall' => ['id' => 'id1', 'name' => 'name1']]],
+        ];
     }
 }

tests/Platform/Bridge/Google/Contract/ToolCallMessageNormalizerTest.php
@@ -1,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpLlm\LlmChain\Tests\Platform\Bridge\Google\Contract;
+
+use PhpLlm\LlmChain\Platform\Bridge\Google\Contract\ToolCallMessageNormalizer;
+use PhpLlm\LlmChain\Platform\Bridge\Google\Gemini;
+use PhpLlm\LlmChain\Platform\Contract;
+use PhpLlm\LlmChain\Platform\Message\ToolCallMessage;
+use PhpLlm\LlmChain\Platform\Model;
+use PhpLlm\LlmChain\Platform\Response\ToolCall;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[Small]
+#[CoversClass(ToolCallMessageNormalizer::class)]
+#[UsesClass(Model::class)]
+#[UsesClass(Gemini::class)]
+#[UsesClass(ToolCallMessage::class)]
+#[UsesClass(ToolCall::class)]
+final class ToolCallMessageNormalizerTest extends TestCase
+{
+    #[Test]
+    public function supportsNormalization(): void
+    {
+        $normalizer = new ToolCallMessageNormalizer();
+
+        self::assertTrue($normalizer->supportsNormalization(new ToolCallMessage(new ToolCall('', '', []), ''), context: [
+            Contract::CONTEXT_MODEL => new Gemini(),
+        ]));
+        self::assertFalse($normalizer->supportsNormalization('not a tool call'));
+    }
+
+    #[Test]
+    public function getSupportedTypes(): void
+    {
+        $normalizer = new ToolCallMessageNormalizer();
+
+        $expected = [
+            ToolCallMessage::class => true,
+        ];
+
+        self::assertSame($expected, $normalizer->getSupportedTypes(null));
+    }
+
+    #[Test]
+    #[DataProvider('normalizeDataProvider')]
+    public function normalize(ToolCallMessage $message, array $expected): void
+    {
+        $normalizer = new ToolCallMessageNormalizer();
+
+        $normalized = $normalizer->normalize($message);
+
+        self::assertEquals($expected, $normalized);
+    }
+
+    /**
+     * @return iterable<array{0: ToolCallMessage, 1: array}>
+     */
+    public static function normalizeDataProvider(): iterable
+    {
+        yield 'scalar' => [
+            new ToolCallMessage(
+                new ToolCall('id1', 'name1', ['foo' => 'bar']),
+                'true',
+            ),
+            [[
+                'functionResponse' => [
+                    'id' => 'id1',
+                    'name' => 'name1',
+                    'response' => ['rawResponse' => 'true'],
+                ],
+            ]],
+        ];
+
+        yield 'structured response' => [
+            new ToolCallMessage(
+                new ToolCall('id1', 'name1', ['foo' => 'bar']),
+                '{"structured":"response"}',
+            ),
+            [[
+                'functionResponse' => [
+                    'id' => 'id1',
+                    'name' => 'name1',
+                    'response' => ['structured' => 'response'],
+                ],
+            ]],
+        ];
+    }
+}

tony-stark-eth/ai-platform

src/Bridge/Google/Contract/ToolNormalizer.php
@@ -1,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Bridge\Google\Contract;
+
+use Symfony\AI\Platform\Bridge\Google\Gemini;
+use Symfony\AI\Platform\Contract\JsonSchema\Factory;
+use Symfony\AI\Platform\Contract\Normalizer\ModelContractNormalizer;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Tool\Tool;
+
+/**
+ * @author Valtteri R <valtzu@gmail.com>
+ *
+ * @phpstan-import-type JsonSchema from Factory
+ */
+final class ToolNormalizer extends ModelContractNormalizer
+{
+    protected function supportedDataClass(): string
+    {
+        return Tool::class;
+    }
+
+    protected function supportsModel(Model $model): bool
+    {
+        return $model instanceof Gemini;
+    }
+
+    /**
+     * @param Tool $data
+     *
+     * @return array{
+     *     functionDeclarations: array{
+     *         name: string,
+     *         description: string,
+     *         parameters: JsonSchema|array{type: 'object'}
+     *     }[]
+     * }
+     */
+    public function normalize(mixed $data, ?string $format = null, array $context = []): array
+    {
+        $parameters = $data->parameters;
+        unset($parameters['additionalProperties']);
+
+        return [
+            'functionDeclarations' => [
+                [
+                    'description' => $data->description,
+                    'name' => $data->name,
+                    'parameters' => $parameters,
+                ],
+            ],
+        ];
+    }
+}

src/Bridge/Google/ModelHandler.php
@@ -14,9 +14,13 @@ namespace Symfony\AI\Platform\Bridge\Google;
 use Symfony\AI\Platform\Exception\RuntimeException;
 use Symfony\AI\Platform\Model;
 use Symfony\AI\Platform\ModelClientInterface;
+use Symfony\AI\Platform\Response\Choice;
+use Symfony\AI\Platform\Response\ChoiceResponse;
 use Symfony\AI\Platform\Response\ResponseInterface as LlmResponse;
 use Symfony\AI\Platform\Response\StreamResponse;
 use Symfony\AI\Platform\Response\TextResponse;
+use Symfony\AI\Platform\Response\ToolCall;
+use Symfony\AI\Platform\Response\ToolCallResponse;
 use Symfony\AI\Platform\ResponseConverterInterface;
 use Symfony\Component\HttpClient\EventSourceHttpClient;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
@@ -59,6 +63,12 @@ final readonly class ModelHandler implements ModelClientInterface, ResponseConve
 
         $generationConfig = ['generationConfig' => $options];
         unset($generationConfig['generationConfig']['stream']);
+        unset($generationConfig['generationConfig']['tools']);
+
+        if (isset($options['tools'])) {
+            $generationConfig['tools'] = $options['tools'];
+            unset($options['tools']);
+        }
 
         return $this->httpClient->request('POST', $url, [
             'headers' => [
@@ -83,11 +93,22 @@ final readonly class ModelHandler implements ModelClientInterface, ResponseConve
 
         $data = $response->toArray();
 
-        if (!isset($data['candidates'][0]['content']['parts'][0]['text'])) {
+        if (!isset($data['candidates'][0]['content']['parts'][0])) {
             throw new RuntimeException('Response does not contain any content');
         }
 
-        return new TextResponse($data['candidates'][0]['content']['parts'][0]['text']);
+        /** @var Choice[] $choices */
+        $choices = array_map($this->convertChoice(...), $data['candidates']);
+
+        if (1 !== \count($choices)) {
+            return new ChoiceResponse(...$choices);
+        }
+
+        if ($choices[0]->hasToolCall()) {
+            return new ToolCallResponse(...$choices[0]->getToolCalls());
+        }
+
+        return new TextResponse($choices[0]->getContent());
     }
 
     private function convertStream(ResponseInterface $response): \Generator
@@ -121,12 +142,68 @@ final readonly class ModelHandler implements ModelClientInterface, ResponseConve
                     throw new RuntimeException('Failed to decode JSON response', 0, $e);
                 }
 
-                if (!isset($data['candidates'][0]['content']['parts'][0]['text'])) {
+                /** @var Choice[] $choices */
+                $choices = array_map($this->convertChoice(...), $data['candidates'] ?? []);
+
+                if (!$choices) {
                     continue;
                 }
 
-                yield $data['candidates'][0]['content']['parts'][0]['text'];
+                if (1 !== \count($choices)) {
+                    yield new ChoiceResponse(...$choices);
+                    continue;
+                }
+
+                if ($choices[0]->hasToolCall()) {
+                    yield new ToolCallResponse(...$choices[0]->getToolCalls());
+                }
+
+                if ($choices[0]->hasContent()) {
+                    yield $choices[0]->getContent();
+                }
             }
         }
+    }
+
+    /**
+     * @param array{
+     *     finishReason?: string,
+     *     content: array{
+     *         parts: array{
+     *             functionCall?: array{
+     *                 id: string,
+     *                 name: string,
+     *                 args: mixed[]
+     *             },
+     *             text?: string
+     *         }[]
+     *     }
+     * } $choice
+     */
+    private function convertChoice(array $choice): Choice
+    {
+        $contentPart = $choice['content']['parts'][0] ?? [];
+
+        if (isset($contentPart['functionCall'])) {
+            return new Choice(toolCalls: [$this->convertToolCall($contentPart['functionCall'])]);
+        }
+
+        if (isset($contentPart['text'])) {
+            return new Choice($contentPart['text']);
+        }
+
+        throw new RuntimeException(\sprintf('Unsupported finish reason "%s".', $choice['finishReason']));
+    }
+
+    /**
+     * @param array{
+     *     id: string,
+     *     name: string,
+     *     args: mixed[]
+     * } $toolCall
+     */
+    private function convertToolCall(array $toolCall): ToolCall
+    {
+        return new ToolCall($toolCall['id'], $toolCall['name'], $toolCall['args']);
     }
 }

tests/Bridge/Google/Contract/AssistantMessageNormalizerTest.php
@@ -12,6 +12,7 @@
 namespace Symfony\AI\Platform\Tests\Bridge\Google\Contract;
 
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
@@ -21,12 +22,14 @@ use Symfony\AI\Platform\Bridge\Google\Gemini;
 use Symfony\AI\Platform\Contract;
 use Symfony\AI\Platform\Message\AssistantMessage;
 use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Response\ToolCall;
 
 #[Small]
 #[CoversClass(AssistantMessageNormalizer::class)]
 #[UsesClass(Gemini::class)]
 #[UsesClass(AssistantMessage::class)]
 #[UsesClass(Model::class)]
+#[UsesClass(ToolCall::class)]
 final class AssistantMessageNormalizerTest extends TestCase
 {
     #[Test]
@@ -49,13 +52,32 @@ final class AssistantMessageNormalizerTest extends TestCase
     }
 
     #[Test]
-    public function normalize(): void
+    #[DataProvider('normalizeDataProvider')]
+    public function normalize(AssistantMessage $message, array $expectedOutput): void
     {
         $normalizer = new AssistantMessageNormalizer();
-        $message = new AssistantMessage('Great to meet you. What would you like to know?');
 
         $normalized = $normalizer->normalize($message);
 
-        self::assertSame([['text' => 'Great to meet you. What would you like to know?']], $normalized);
+        self::assertSame($expectedOutput, $normalized);
+    }
+
+    /**
+     * @return iterable<string, array{AssistantMessage, array{text?: string, functionCall?: array{id: string, name: string, args?: mixed}}[]}>
+     */
+    public static function normalizeDataProvider(): iterable
+    {
+        yield 'assistant message' => [
+            new AssistantMessage('Great to meet you. What would you like to know?'),
+            [['text' => 'Great to meet you. What would you like to know?']],
+        ];
+        yield 'function call' => [
+            new AssistantMessage(toolCalls: [new ToolCall('id1', 'name1', ['arg1' => '123'])]),
+            [['functionCall' => ['id' => 'id1', 'name' => 'name1', 'args' => ['arg1' => '123']]]],
+        ];
+        yield 'function call without parameters' => [
+            new AssistantMessage(toolCalls: [new ToolCall('id1', 'name1')]),
+            [['functionCall' => ['id' => 'id1', 'name' => 'name1']]],
+        ];
     }
 }

tests/Bridge/Google/Contract/ToolCallMessageNormalizerTest.php
@@ -1,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Tests\Bridge\Google\Contract;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Platform\Bridge\Google\Contract\ToolCallMessageNormalizer;
+use Symfony\AI\Platform\Bridge\Google\Gemini;
+use Symfony\AI\Platform\Contract;
+use Symfony\AI\Platform\Message\ToolCallMessage;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\Response\ToolCall;
+
+#[Small]
+#[CoversClass(ToolCallMessageNormalizer::class)]
+#[UsesClass(Model::class)]
+#[UsesClass(Gemini::class)]
+#[UsesClass(ToolCallMessage::class)]
+#[UsesClass(ToolCall::class)]
+final class ToolCallMessageNormalizerTest extends TestCase
+{
+    #[Test]
+    public function supportsNormalization(): void
+    {
+        $normalizer = new ToolCallMessageNormalizer();
+
+        self::assertTrue($normalizer->supportsNormalization(new ToolCallMessage(new ToolCall('', '', []), ''), context: [
+            Contract::CONTEXT_MODEL => new Gemini(),
+        ]));
+        self::assertFalse($normalizer->supportsNormalization('not a tool call'));
+    }
+
+    #[Test]
+    public function getSupportedTypes(): void
+    {
+        $normalizer = new ToolCallMessageNormalizer();
+
+        $expected = [
+            ToolCallMessage::class => true,
+        ];
+
+        self::assertSame($expected, $normalizer->getSupportedTypes(null));
+    }
+
+    #[Test]
+    #[DataProvider('normalizeDataProvider')]
+    public function normalize(ToolCallMessage $message, array $expected): void
+    {
+        $normalizer = new ToolCallMessageNormalizer();
+
+        $normalized = $normalizer->normalize($message);
+
+        self::assertEquals($expected, $normalized);
+    }
+
+    /**
+     * @return iterable<array{0: ToolCallMessage, 1: array}>
+     */
+    public static function normalizeDataProvider(): iterable
+    {
+        yield 'scalar' => [
+            new ToolCallMessage(
+                new ToolCall('id1', 'name1', ['foo' => 'bar']),
+                'true',
+            ),
+            [[
+                'functionResponse' => [
+                    'id' => 'id1',
+                    'name' => 'name1',
+                    'response' => ['rawResponse' => 'true'],
+                ],
+            ]],
+        ];
+
+        yield 'structured response' => [
+            new ToolCallMessage(
+                new ToolCall('id1', 'name1', ['foo' => 'bar']),
+                '{"structured":"response"}',
+            ),
+            [[
+                'functionResponse' => [
+                    'id' => 'id1',
+                    'name' => 'name1',
+                    'response' => ['structured' => 'response'],
+                ],
+            ]],
+        ];
+    }
+}